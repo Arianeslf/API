b5ac19db6d6b04f839cd8f663715cc7a7423309c
Ariane Silvestre <arianesilvetremaira73@gmail.com>
2026-05-15 19:49:27 -0300

    added 31 files

---
+++ server.js
@@ -0,0 +1,76 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "./generated/prisma/client.js";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/usuarios", async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        age: parseInt(req.body.age),
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const { name, email, age } = req.query;
+
+    const users = await prisma.user.findMany({
+      where: {
+        ...(name && { name }),
+        ...(email && { email }),
+        ...(age && { age: parseInt(age) }),
+      },
+    });
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.idade,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  try {
+    await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(204).send("Usario deletado com sucesso");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
